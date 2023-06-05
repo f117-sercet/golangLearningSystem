cfce4cb1780ff28a6d746cdf691459945314c148
sowrd <15159885771@163.com>
2023-06-05 18:17:15 +0800

    切片的注意细节

--- goproject/go_code/project13/sliceUseType/main.go
+++ goproject/go_code/project13/sliceUseType/main.go
@@ -31,5 +31,17 @@ func main() {
 
 		fmt.Printf("i=%v v=%v \n", i, v)
 	}
-	// slice 切片学习
+	/**
+	  1. 切片初始化时，var slice = arr[StartIndex:endIndex]
+	     说明：从arr数组下标为startIndex，取到下标为endIndex的元素（不包含arr[endIndex]）
+	  2. 切片初始化时，仍然不能越界。范围在[0-len(arr)]之间，但是可以动态增长
+	     1. var slice = arr[0:end],可以简写为 var slice = arr[:end]
+	     2. var slice = arr[start:len(arr)] 可以简写：var slice = arr[start:]
+	     3. var slice = arr[0:len(arr)] 可以简写：var slice = arr[:]
+	  3. cap是一个内置函数，用于统计切片的容量，即最大可以存放多少个元素
+	  4. 切片定义完之后，还不能使用，因为本身是一个空的，需要让其引用到一个数组，或者make一个空间供切片来使用。
+	  5. 切片可以继续使用切片
+
+	*/
+
 }
