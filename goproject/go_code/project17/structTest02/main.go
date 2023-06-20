fadcb676c9c540413380099eea34deb0f184257a
sowrd <15159885771@163.com>
2023-06-20 08:51:54 +0800

    结构体声明的4种方法

--- goproject/go_code/project17/structTest02/main.go
+++ goproject/go_code/project17/structTest02/main.go
@@ -14,4 +14,25 @@ func main() {
 	p1 := Person{"Marry", 20}
 
 	fmt.Println(p1)
+
+	// 方式三：
+	var p3 *Person = new(Person)
+	(*p3).Name = "tom"
+	(*p3).Age = 18
+	fmt.Println(*p3)
+
+	// 方式3简化
+
+	var p4 *Person = new(Person)
+	p4.Name = "toms"
+	(*p3).Age = 19
+	fmt.Println("方式三简化", p4)
+
+	// 方式4
+	var person *Person = &Person{}
+
+	person.Name = "王二"
+	person.Age = 26
+
+	fmt.Print("方式4", *person)
 }
