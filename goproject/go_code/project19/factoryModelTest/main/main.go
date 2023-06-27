0f72bbd4936fbdc51a388e2a2a2323ce70af4bdf
sowrd <15159885771@163.com>
2023-06-27 15:25:07 +0800

    工厂模式，程序调整

--- goproject/go_code/project19/factoryModelTest/main/main.go
+++ goproject/go_code/project19/factoryModelTest/main/main.go
@@ -1,20 +1,21 @@
 package main
 
-import "fmt"
-
-/*import "goproject/go_code/project19/factoryModelTest/model"*/
+import (
+	"fmt"
+	"golangLearningSystem/goproject/go_code/project19/factoryModelTest/model"
+)
 
 func main() {
 
-	/*	stu := model.Student{
-			Name: "tom",
-			Age:  18,
-		}
-		fmt.Println(stu)*/
+	stu := model.Student{
+		Name: "tom",
+		Age:  18,
+	}
+	fmt.Println(stu)
 	fmt.Println(123)
 
-	// 工厂模式
-	/*student := model.NewStudent("小王", 18)
-	fmt.Println(*student)*/
+	//工厂模式
+	student := model.NewStudent("小王", 18)
+	fmt.Println(*student)
 
 }
