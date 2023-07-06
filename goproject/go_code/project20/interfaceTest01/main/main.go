d04080421450924f7d3a92b0bcecf0088ee6b231
sowrd <15159885771@163.com>
2023-07-06 08:49:45 +0800

    接口特点和入门

--- goproject/go_code/project20/interfaceTest01/main/main.go
+++ goproject/go_code/project20/interfaceTest01/main/main.go
@@ -11,6 +11,8 @@ type Usb interface {
 
 type Phone struct {
 }
+type Computer struct {
+}
 
 // 让Phone 实现usb接口的方法
 func (p Phone) Start() {
@@ -21,6 +23,21 @@ func (p Phone) Stop() {
 	fmt.Println("手机停止工作")
 }
 
+// 只要是实现了USB接口，指实现了usb接口声明的所有方法
+func (c Computer) Working(usb Usb) {
+	usb.Stop()
+	usb.Start()
+}
+
 func main() {
+	var phone Phone
+	phone.Start()
+	phone.Stop()
+
+	var computer Computer
+	computer.Working(phone)
+	computer.Working(phone)
+
+	//fmt.Println(Phones())
 
 }
