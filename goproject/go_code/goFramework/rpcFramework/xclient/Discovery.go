0dae0728c531250ff518fd59c4043e0427bf52a9
sowrd <15159885771@163.com>
2023-10-24 11:56:50 +0800

    服务注册

---
+++ goproject/go_code/goFramework/rpcFramework/xclient/Discovery.go
@@ -0,0 +1,139 @@
+package xclient
+
+import (
+	"errors"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+)
+
+type SelectMode int
+
+const (
+	RandomSelect SelectMode = iota
+	RoundRobinSelect
+)
+
+type Discovery interface {
+	Refresh() error
+	Update(servers []string) error
+	Get(mode SelectMode) (string, error)
+	GetAll() ([]string, error)
+}
+
+type MultiServersDiscovery struct {
+	r       *rand.Rand
+	mu      sync.RWMutex
+	servers []string
+	index   int
+}
+
+type GeeRegistryDiscovery struct {
+	*MultiServersDiscovery
+	registry   string
+	timeout    time.Duration
+	lastUpdate time.Time
+}
+
+const defaultUpdateTimeout = time.Second * 10
+
+func NewMultiServerDiscovery(servers []string) *MultiServersDiscovery {
+
+	d := &MultiServersDiscovery{
+		servers: servers,
+		r:       rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+
+	d.index = d.r.Intn(math.MaxInt32 - 1)
+	return d
+}
+
+func NewGeeRegistryDiscovery(registerAddr string, timeout time.Duration) *GeeRegistryDiscovery {
+
+	if timeout == 0 {
+		timeout = defaultUpdateTimeout
+	}
+	d := &GeeRegistryDiscovery{
+		MultiServersDiscovery: NewMultiServerDiscovery(make([]string, 0)),
+		registry:              registerAddr,
+		timeout:               timeout,
+	}
+	return d
+}
+
+func (d *GeeRegistryDiscovery) Update(Servers []string) error {
+
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.servers = Servers
+	d.lastUpdate = time.Now()
+	return nil
+}
+
+func (d *GeeRegistryDiscovery) Refresh() error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	if d.lastUpdate.Add(d.timeout).After(time.Now()) {
+		return nil
+	}
+	log.Println("rpc registry: refresh servers from registry", d.registry)
+	resp, err := http.Get(d.registry)
+	if err != nil {
+		log.Println("rpc registry refresh err:", err)
+		return err
+	}
+	servers := strings.Split(resp.Header.Get("X-rpc-Servers"), ",")
+	d.servers = make([]string, 0, len(servers))
+	for _, server := range servers {
+		if strings.TrimSpace(server) != "" {
+			d.servers = append(d.servers, strings.TrimSpace(server))
+		}
+	}
+	d.lastUpdate = time.Now()
+	return nil
+}
+func (d *GeeRegistryDiscovery) Get(mode SelectMode) (string, error) {
+	if err := d.Refresh(); err != nil {
+		return "", err
+	}
+	return d.MultiServersDiscovery.Get(mode)
+}
+
+func (d *GeeRegistryDiscovery) GetAll() ([]string, error) {
+	if err := d.Refresh(); err != nil {
+		return nil, err
+	}
+	return d.MultiServersDiscovery.GetAll()
+}
+
+func (d MultiServersDiscovery) GetAll() ([]string, error) {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+	// return a copy of d.servers
+	servers := make([]string, len(d.servers), len(d.servers))
+	copy(servers, d.servers)
+	return servers, nil
+}
+
+func (d *MultiServersDiscovery) Get(mode SelectMode) (string, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	n := len(d.servers)
+	if n == 0 {
+		return "", errors.New("rpc discovery: no available servers")
+	}
+	switch mode {
+	case RandomSelect:
+		return d.servers[d.r.Intn(n)], nil
+	case RoundRobinSelect:
+		s := d.servers[d.index%n]
+		d.index = (d.index + 1) % n
+		return s, nil
+	default:
+		return "", errors.New("rpc discovery: not supported select mode")
+	}
+}
